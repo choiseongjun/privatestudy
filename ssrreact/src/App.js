7f97d35fbeb8147e1bf5c29a034f783ce89dcf6d
최성준 <csj2702@naver.com>
2020-10-04 20:08:04 +0900

    first commit

---
+++ ssrreact/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  const[count,setCount] = React.useState(0);
+
+  const increment = () =>{
+    setCount(count+1);
+  }
+  const decrement = () =>{
+    setCount(count-1);
+  }
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
